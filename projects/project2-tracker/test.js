fb05b9718bf8fc52f749b772e1b5906a5df380a6
snelson22 <s.nelson22@tcu.edu>
2025-11-04 14:13:02 -0600

    Add Project 2 tracker to projects folder

---
+++ projects/project2-tracker/test.js
@@ -0,0 +1,10 @@
+// console.log("Hello from Node.js!");
+
+//  // Try some basic JavaScript
+let numbers = [1, 2, 3, 4, 5];
+let doubled = numbers.map(n => n * 2);
+console.log("Doubled numbers:", doubled);
+
+// // Calculate a sum
+let sum = numbers.reduce((acc, n) => acc + n, 0);
+console.log("Sum:", sum);
